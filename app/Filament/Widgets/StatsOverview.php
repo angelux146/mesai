55a01fb103bd87503ded376ff1ab184e589fe952
angelux146 <angeldejesusgarcialopez@gmail.com>
2026-05-13 19:35:48 -0600

    dashboard redesign listo

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Menu;
+use App\Models\Cliente;
+use App\Models\Reservacion;
+use Filament\Widgets\Widget;
+
+class StatsOverview extends Widget
+{
+    protected string $view = 'filament.widgets.stats-overview';
+
+    protected int|string|array $columnSpan = 'full';
+
+    public function getData(): array
+    {
+        return [
+
+            'stats' => [
+
+                [
+                    'title' => '🍔 Platillos',
+                    'value' => Menu::count(),
+                    'description' => 'Platillos registrados',
+                ],
+
+                [
+                    'title' => '👥 Clientes',
+                    'value' => Cliente::count(),
+                    'description' => 'Clientes registrados',
+                ],
+
+                [
+                    'title' => '📅 Reservaciones',
+                    'value' => Reservacion::count(),
+                    'description' => 'Reservaciones totales',
+                ],
+
+                [
+                    'title' => '🔥 Hoy',
+                    'value' => Reservacion::whereDate('fecha', today())->count(),
+                    'description' => 'Reservaciones de hoy',
+                ],
+
+            ]
+
+        ];
+    }
+}
